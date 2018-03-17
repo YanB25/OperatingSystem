c8a231c42abed9f0086c1bdbb439f95bf9e6dc3e
YanB25 <442826556@qq.con>
2018-03-17 23:59:44 +0800

    delete comment in userCodes/build.sh

--- step_2/userCodes/build.sh
+++ step_2/userCodes/build.sh
@@ -5,9 +5,3 @@ nasm stone.asm -o ur.bin -d DEBUG -d UR
 nasm stone.asm -o dl.bin -d DEBUG -d DL
 nasm stone.asm -o dn.bin -d DEBUG -d DN
 nasm stone.asm -o dr.bin -d DEBUG -d DR
-# dd if=ul.bin of=boot.img conv=notrunc oflag=seek_bytes seek="$((512*0))"
-# dd if=up.bin of=boot.img conv=notrunc oflag=seek_bytes seek="$((512*1))"
-# dd if=ur.bin of=boot.img conv=notrunc oflag=seek_bytes seek="$((512*2))"
-# dd if=dl.bin of=boot.img conv=notrunc oflag=seek_bytes seek="$((512*3))"
-# dd if=dn.bin of=boot.img conv=notrunc oflag=seek_bytes seek="$((512*4))"
-# dd if=dr.bin of=boot.img conv=notrunc oflag=seek_bytes seek="$((512*5))"
