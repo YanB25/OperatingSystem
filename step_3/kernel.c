c2eb86b35b3b4846697f98feb955c4a138828039
YanB25 <442826556@qq.con>
2018-03-20 20:14:28 +0800

    finish kernel. c and kernel.h - generate to kernel.bin - used as kernel of os

---
+++ step_3/kernel.c
@@ -0,0 +1,8 @@
+#include "kernel.h"
+#include "utilities.h"
+int main() {
+
+    print_promt();
+    while(1) {}
+    return 0;
+}
---
+++ step_3/kernel.h
@@ -0,0 +1,10 @@
+#ifndef __KERNEL_H_
+#define __KERNEL_H_
+__asm__(".code16gcc\n");
+ __asm__(".globl _start\n");
+__asm__("_start:\n");
+__asm__("mov $0, %eax\n");
+__asm__("mov %ax, %ds\n");
+__asm__("mov %ax, %es\n");
+__asm__("jmpl $0, $main\n");
+#endif
