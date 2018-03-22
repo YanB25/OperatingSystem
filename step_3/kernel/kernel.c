9fb63a9382fe2a837390fad55552d15c3497972d
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-03-22 11:13:03 +0800

    change kernel/makefile and include/makefile - kernel/makefile to fit kernel.c dependency - include/makefile to fix trival protencial bug

--- step_3/kernel/kernel.c
+++ step_3/kernel/kernel.c
@@ -1,6 +1,7 @@
 #include "kernel.h"
 #include "../include/utilities.h"
 #include "../user/user.h"
+#include "../user/terminal.h"
 #define true 1
 #define false 0
 int main() {
@@ -13,12 +14,6 @@ int main() {
     draw_str("I love you", 10, 8);
     putln("can i still output?");
     jumpToUser();
-    while(true) {
-        int try = kbhit();
-        if (try) {
-            int key = readkb();
-            putch(key);
-        }
-    }
+    terminal();
     return 0;
 }
