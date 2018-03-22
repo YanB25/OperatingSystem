898de0bff88fd737ac1f63e77fd6bf256078aecf
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-03-22 12:05:52 +0800

    success in framwork. success in terminel. - change dependency. kernel depend on terminal. terminal depend on stone - change mkfile

--- step_3/kernel/kernel.c
+++ step_3/kernel/kernel.c
@@ -2,20 +2,18 @@
 #include "../include/utilities.h"
 #include "../user/user.h"
 #include "../user/terminal.h"
-#include "../user/stone.h"
 #define true 1
 #define false 0
 int main() {
     clear_screen();
-    int col = sum(10);
-    // int col=20;
-    draw_str("hello world", 5, col);
-    putln("I am here!");
-    putln("hei, i am happy!");
-    draw_str("I love you", 10, 8);
-    putln("can i still output?");
-    jumpToUser();
-    // terminal();
-    stone();
+    // int col = sum(10);
+    // // int col=20;
+    // draw_str("hello world", 5, col);
+    // putln("I am here!");
+    // putln("hei, i am happy!");
+    // draw_str("I love you", 10, 8);
+    // putln("can i still output?");
+    // jumpToUser();
+    terminal();
     return 0;
 }
--- step_3/user/terminal.c
+++ step_3/user/terminal.c
@@ -1,11 +1,23 @@
 #include "../include/utilities.h"
+#include "stone.h"
+const char* PROMT = "yb@yb-thinkpad-e450:~\% ";
 int terminal() {
-    puts("yb@yb-thinkpad-e450:~\% ");
+    puts(PROMT);
     while(1) {
         int try = kbhit();
         if (try) {
             int key = readkb();
-            putch(key);
+            if (key == 'q') {
+                stone();
+            }
+            if (key == '\r') {
+                putch('\n');
+                putch('\r');
+                // putch('\r');
+                puts(PROMT);
+            } else {
+                putch(key);
+            }
         }
     }
     return 0;
