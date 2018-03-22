7106bc9cfcf50150f6e7f6acfa813d867dbfbe17
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-03-22 13:27:12 +0800

    build up framework in terminal

--- step_3/user/terminal.c
+++ step_3/user/terminal.c
@@ -1,39 +1,68 @@
 #include "../include/utilities.h"
 #include "stone.h"
+
 #define BACK_SPACE 8
-const char* PROMT = "yb@yb-thinkpad-e450:~$ ";
+#define BUFFER_SIZE 64
+#define PROMT "yb@yb-thinkpad-e450:~$ "
+#define HELP_MSG "welcome to YB command line\n\r"\
+    "enter q, w, e, a, s, d to run progran"
+
+char CMD_BUFFER[BUFFER_SIZE + 10] = {};
+void parseCMD(int );
+
 int terminal() {
     puts(PROMT);
     int offsetx = 0;
     int offsety = 0;
+    int CMDindex = 0;
+
     while(1) {
         int try = kbhit();
-        if (try) {
-            int key = readkb();
-            switch(key) {
-                case('\r'):
-                    putch('\n');
-                    putch('\r');
-                    // putch('\r');
-                    puts(PROMT);
-                    offsetx = 0;
-                    offsety++;
-                break;
-                case(BACK_SPACE):
-                    if (offsetx > 0) {
-                        putch(8);
-                        putch(' ');
-                        putch(8);
-                        offsetx--;
-                    }
-                break;
-                case('\t'):
-                break;
-                default:
-                    putch(key);
-                    offsetx++;
-            }
+        if (!try) continue;
+
+        int key = readkb();
+        switch(key) {
+            case('\r'):
+                putch('\n');
+                putch('\r');
+
+                offsetx = 0;
+                offsety++;
+
+                parseCMD(CMDindex);
+                puts(PROMT);
+
+                CMDindex = 0;
+                CMD_BUFFER[CMDindex] = 0;
+
+            break;
+            case(BACK_SPACE):
+                if (offsetx > 0) {
+                    putch(8);
+                    putch(' ');
+                    putch(8);
+                    offsetx--;
+
+                    CMDindex--;
+                    CMD_BUFFER[CMDindex] = 0;
+                }
+            break;
+            case('\t'):
+            break;
+            default:
+                putch(key);
+                offsetx++;
+                CMD_BUFFER[CMDindex++] = key;
+                CMD_BUFFER[CMDindex] = 0;
         }
     }
     return 0;
+}
+
+void parseCMD(int CMDindex) {
+    if (CMDindex == 1 && CMD_BUFFER[0] == 'q') {
+        stone();
+    } else {
+        putln(HELP_MSG);
+    }
 }
