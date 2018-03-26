6bebf15984be053af17fbba1ea57468c9e29e17f
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-03-26 23:11:28 +0800

    try to use intel syntax instead of att I don't know if where are bugs.

--- step_3/kernel/kernel.h
+++ step_3/kernel/kernel.h
@@ -1,10 +1,12 @@
 #ifndef __KERNEL_H_
 #define __KERNEL_H_
 // __asm__(".code16gcc\n");
+__asm__(".intel_syntax noprefix\n");
 __asm__(".globl _start\n");
 __asm__("_start:\n");
-__asm__("mov $0, %eax\n");
-__asm__("mov %ax, %ds\n");
-__asm__("mov %ax, %es\n");
-__asm__("jmpl $0, $main\n");
+__asm__("mov eax, 0\n");
+__asm__("mov ds, ax\n");
+__asm__("mov es, ax\n");
+__asm__("jmp 0: main\n");
+__asm__(".att_syntax\n");
 #endif
