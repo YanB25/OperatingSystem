e29a5cd9f6ebe7eaf38691ae4815994f3570db2f
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-03-28 09:29:14 +0800

    necessary change: move struct declaration from datablock.c to .h

--- step_3/filesystem/FATMacro.h
+++ step_3/filesystem/FATMacro.h
@@ -1,5 +1,23 @@
 #ifndef __FAT_MACRO_H_
 #define __FAT_MACRO_H_
+#include <stdint.h>
+struct FAT_ITEM {
+    char filename[8];
+    char extendname[3];
+    uint8_t mod;
+    
+    uint8_t res; //reserved
+    uint8_t created_time; // ms
+    uint16_t hms; // hour minute second
+    uint16_t ymd; // year month day
+    uint16_t recent_access_ymd;
+    uint16_t bhigh_cluster;
+
+    uint16_t modify_hms;
+    uint16_t modify_ymd;
+    uint16_t blow_cluster;
+    uint32_t filesize; // in bytes
+}__attribute__((packed));
 
 #define FAT_rw 0
 #define FAT_ro 1
