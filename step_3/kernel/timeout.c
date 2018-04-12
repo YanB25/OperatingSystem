0e6ef070c9aa4e99e7938d4f7cf2680976038204
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-04-12 21:12:33 +0800

    [feature] add running lighting in terminal. it is ugly but it shows that I grasp how to use interupt porperly. - timeout.c: add certain function

--- step_3/kernel/timeout.c
+++ step_3/kernel/timeout.c
@@ -2,31 +2,31 @@
 #include "../include/stdio.h"
 char ch = 'A';
 #define DELAY 2
-#define UpBound 12
-#define DownBound 13
-int16_t up = 0;
-int16_t down = 0;
-int16_t delay = DELAY;
+int delX[] = {1, 0, -1, 0};
+int delY[] = {0, 1, 0, -1};
+int direction = 0;
+int x = 0;
+int y = 0;
+uint8_t style = 0;
 void callback();
+char nextAlphabet(char);
 void timeout() {
-    _draw_char(ch++, 0, 0x0F);
+    // _draw_char(ch++, 0, 0x0F);
     callback();
 }
 void callback() {
-    if (delay --> 0) return;
-    delay = DELAY;
-    if (up - 1 >= 0) {
-        draw_char_style(' ', up-1, 78, DEFAULT_STYLE);
-    } else {
-        draw_char_style(' ', UpBound - 1, 78, DEFAULT_STYLE);
-    }
-    if (down + 1 <= 24) {
-        draw_char_style(' ', down+1, 78, DEFAULT_STYLE);
-    } else {
-        draw_char_style(' ', DownBound , 78, DEFAULT_STYLE);
-    }
-    draw_char_style(' ', up, 78, G_BG_WHITE | G_DARK);
-    draw_char_style(' ', down, 78, G_BG_WHITE | G_DARK);
-    up = (up+1) % UpBound;
-    down = (down-1) < DownBound ? 24 : down-1;
+    style = (style+1) & 0b00001111;
+    ch = nextAlphabet(ch);
+    draw_char_style(ch, y, x, style);
+    if (y == 0 && x == 0) direction = 0;
+    if (y == 0 && x == 79) direction = 1;
+    if (y == 24 && x == 79) direction = 2;
+    if (y == 24 && x == 0) direction = 3;
+    y += delY[direction];
+    x += delX[direction];
+}
+char nextAlphabet(char c) {
+    if (c == 'z') return 'A';
+    if (c == 'Z') return 'a';
+    return c+1;
 }
