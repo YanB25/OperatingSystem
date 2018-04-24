a433e973b954ca07f879a53aca561f57e4a77e6b
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-04-24 19:36:05 +0800

    [rename] PCBManager.h -> PCBManager.c - PCBManage.c: turn its implement into C file. so that aseembly can call   the function

--- step_3/kernel/PCBManager.c
+++ step_3/kernel/PCBManager.c
@@ -1,5 +1,3 @@
-#ifndef __PCB_MANAGER_H_
-#define __PCB_MANAGER_H_
 #include <stdint.h>
 #include "PCB.h"
 /**
@@ -25,8 +23,28 @@ void init_PCBManager() {
     PCB_manager.psize = 0;
     PCB_manager.cur_active = -1;
 }
+/**
+ * @biref testcase
+ * 
+ * this is only a testcase
+ * TODO:please delete me after debug finish
+ */
+void init_testcase() {
+    PCB_manager.psize = 1;
+    PCB_manager.cur_active = 0;
+    struct RegisterImage* ri = &(PCB_manager.PCBList[PCB_manager.cur_active].register_image);
+    ri->sp = 1;
+    ri->ss = 2;
+    ri->gs = 3;
+    ri->fs = 4;
+    ri->es = 5;
+    ri->ds = 6;
+    ri->ax = 0x1234;
+    ri->flags = 0x5678;
+}
 void* get_current_PCB_address() {
     int16_t index = PCB_manager.cur_active;
-    return (void*) &(PCB_manager.PCBList[index]);
+    struct RegisterImage* ret = &(PCB_manager.PCBList[index].register_image); ///< typecheck for safe.
+    return (void*) ret;
+
 }
-#endif
