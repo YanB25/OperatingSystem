5ef8dc62315205aee6709b00086ce3fe8300ab0c
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-05-22 01:53:37 +0800

    modify linux version complete in enter pm

---
+++ linux/boot/boot.s
@@ -0,0 +1,135 @@
+BITS 16
+SYSSIZE equ 0x3000
+SETUPLEN equ 4 ; in fact it is num of sectors of head.s
+BOOTSEG equ 0x07c0
+INITSEG equ 0x9000
+SETUPSEG equ 0x9020
+SYSSEG equ 0x1000
+ENDSEG equ SYSSEG + SYSSIZE
+
+global _start
+
+jmp BOOTSEG: _start
+_start:
+    mov ax, BOOTSEG
+    mov ds, ax
+    mov ax, INITSEG
+    mov es, ax
+    mov cx, 256
+    xor si, si
+    xor di, di
+    rep movsw
+    jmp INITSEG: go
+go:
+    mov ax, cs
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0xFF00
+load_setup: ; in fact it is `load head.s`
+    mov dx, 0x0000
+    mov cx, 0x0002
+    mov bx, 0x0200
+    mov ax, 0x0200 + SETUPLEN
+    int 0x13
+
+ok_load_setup: ; no error handler.
+
+    ;print some msg
+    mov ah, 0x03
+    xor bh, bh
+    int 0x10
+    mov cx, 24
+    mov bx, 0x0007
+    mov bp, msg1
+    mov ax, 0x1301
+    int 0x10
+
+    ; load system at 0x10000
+    mov ax, SYSSEG  
+    mov es, ax ; segment of 0x010000
+    call read_it
+
+    jmp SETUPSEG: 0
+
+data:
+sread dw 1+SETUPLEN
+head dw 0
+track dw 0
+
+read_it:
+    mov ax, es
+;     test ax, 0x0fff
+; die:
+;     jne die
+    xor bx, bx
+rp_read:
+    mov ax, es
+    cmp ax, ENDSEG
+    jb ok1_read
+    ret
+
+ok1_read:
+    mov ax, SECTORS
+    sub ax, [sread]
+    mov cx, ax
+    shl cx, 9
+    add cx, bx
+    jnc ok2_read
+    je ok2_read
+    xor ax, ax
+    sub ax, bx
+    shr ax, 9
+ok2_read:
+    call read_track
+    mov cx, ax
+    add ax, [sread]
+    cmp ax, SECTORS
+    jne ok3_read
+    mov ax, 1
+    sub ax, [head]
+    jne ok4_read
+
+    inc word [track]
+ok4_read:
+    mov [head] ,ax
+    xor ax,ax
+ok3_read:
+    mov [sread], ax
+    shl cx, 9
+    add bx, cx
+    jnc rp_read
+    mov ax, es
+    add ax, 0x1000
+    mov es, ax
+    xor bx, bx
+    jmp rp_read
+read_track:
+    push ax
+    push bx
+    push cx
+    push dx
+    mov dx, [track]
+    mov cx, [sread]
+    inc cx
+    mov ch, dl
+    mov dx, [head]
+    mov dh, dl
+    mov dl, 0
+    and dx, 0x0100
+    mov ah, 2
+    int 0x13
+    jc bad_rt
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+bad_rt:
+    jmp $
+SECTORS equ 18
+
+msg1 db "loading system", 0
+
+times 510 - ($-$$) db 0
+dw 0xAA55
---
+++ linux/boot/setup.s
@@ -0,0 +1,101 @@
+[BITS 16]
+INITSEG equ 0x9000
+SYSSEG equ 0x1000
+SETUPSEG equ 0x9020
+%macro DELAY 0
+    dw 0x00eb 
+    dw 0x00eb
+%endmacro
+start:
+    mov ax, INITSEG
+    mov ds, ax
+    mov ah, 0x03
+    xor bh, bh
+
+    ;save the cursor
+    int 0x10
+    mov [0], dx
+
+    ;
+    mov ah, 0x88
+    int 0x15
+    mov [2], ax
+
+    ; move to pm!!
+    cli
+
+    mov ax, 0x0000
+    cld
+do_move:
+    mov es, ax
+    add ax, 0x1000
+    cmp ax, 0x9000
+    jz end_move
+    mov ds, ax
+    xor di, di
+    xor si, si
+    mov cx, 0x8000
+    rep movsw
+    jmp do_move
+end_move:
+    mov ax, SETUPSEG
+    mov ds, ax ;TODO: ??
+
+    lidt [idt_48]
+    lgdt [gdt_48]
+
+    ;NOTICE: not origin implement
+    in al, 92H
+    or al, 00000010b
+    out 92h, al
+
+    ; 8259A program
+    mov al, 0x11
+    out 0x20, al
+    DELAY
+    out 0xA0, al
+    DELAY
+    mov al, 0x20
+    out 0x21, al
+    DELAY
+    mov al, 0x28
+    out 0xA1, al
+    DELAY
+    mov al, 0x04
+    out 0x21, al
+    DELAY
+    mov al, 0x02
+    out 0xA1, al
+    DELAY
+    mov al, 0x01
+    out 0x21, al
+    DELAY
+    out 0xA1, al
+    DELAY
+    mov al, 0xFF
+    out 0x21, al
+    DELAY
+    out 0xA1, al
+
+    ; cr0 set pe
+    mov ax, 0x0001
+    lmsw ax
+    jmp 8:0
+
+GDT:
+    dw 0, 0, 0, 0
+    dw 0x07FF
+    dw 0x0000
+    dw 0x9A00
+    dw 0x00C0
+    dw 0x07FF
+    dw 0x0000
+    dw 0x9200
+    dw 0x00C0
+idt_48:
+    dw 0, 0, 0
+gdt_48:
+    dw 0x800
+    dw 512+GDT, 0x9
+
+    jmp $
